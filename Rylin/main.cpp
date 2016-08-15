8b7b5a6e2993eaec01d5cb021f2f3e2209d26e4a
Sinketsu <ssolloss@yandex.ru>
2016-08-15 11:11:35 +0500

    add libs to project

--- Rylin/main.cpp
+++ Rylin/main.cpp
@@ -1,9 +1,12 @@
-#include <iostream>
+#include <BearLibTerminal.h>
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    terminal_open();
+    terminal_set("window: title='RYLIN'");
+    terminal_refresh();
+    while(true);
     return 0;
 }
