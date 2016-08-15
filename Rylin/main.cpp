975c3680cc90d495e34f1126bcf400ea1301b849
Sinketsu <ssolloss@yandex.ru>
2016-08-15 17:10:34 +0500

    change sizof window 160x50 -> 170x50

--- Rylin/main.cpp
+++ Rylin/main.cpp
@@ -9,21 +9,17 @@ using namespace std;
 void init()
 {
     terminal_open();
-    terminal_set("window: title='RYLIN', size=160x50");
+    terminal_set("window: title='RYLIN', size=170x50");
     terminal_set("font: fonts/UbuntuMono-R.ttf, size=12");
     terminal_set("terminal.encoding=1251");
 }
-
 
 
 int main()
 {
     init();
 
-    if (Player::name == "")
-    {
-        start_menu();
-    }
+    start_menu();
 
     while (true)
     {
