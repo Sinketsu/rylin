df75efdd2fba47564bc291f12564debc1b5584db
Sinketsu <ssolloss@yandex.ru>
2016-08-15 11:46:01 +0500

    add initialization

--- Rylin/main.cpp
+++ Rylin/main.cpp
@@ -2,11 +2,22 @@
 
 using namespace std;
 
-int main()
+void init()
 {
     terminal_open();
-    terminal_set("window: title='RYLIN'");
+    terminal_set("window: title='RYLIN', size=160x50");
+    terminal_set("font: fonts/UbuntuMono-R.ttf, size=12");
+    terminal_set("terminal.encoding=1251");
+}
+
+int main()
+{
+    init();
+    terminal_print(1, 1, "Hello!");
+    terminal_print(1, 2, "Привет!");
     terminal_refresh();
-    while(true);
+
+    while(terminal_read() != TK_ESCAPE);
+    terminal_close();
     return 0;
 }
